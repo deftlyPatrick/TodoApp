ed27e8ec11ae1a3167443652c4cfcca76fbfe020
Patrick <wongpatrick98@gmail.com>
2018-10-25 00:43:43 -0700

    Add files via upload

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Patrick Wong on 10/24/18.
+//  Copyright © 2018 Patrick Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TodoApp/ViewController/TodoTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TodoTableViewController.swift
+//  TodoApp
+//
+//  Created by Patrick Wong on 10/25/18.
+//  Copyright © 2018 Patrick Wong. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
